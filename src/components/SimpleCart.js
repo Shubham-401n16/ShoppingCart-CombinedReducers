54fbd695ac8f3cc803473bdb2b96ce99b0a5973d
Shubham <smajumdar22@gmail.com>
2020-07-19 10:16:50 -0700

    Update

--- src/components/SimpleCart.js
+++ src/components/SimpleCart.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { Grid, Typography, Button, Accordion, AccordionSummary, AccordionDetails } from '@material-ui/core';
-import {Delete} from '@material-ui/icons';
+import { Delete } from '@material-ui/icons';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore'
 
 
@@ -10,24 +10,21 @@ function SimpleCart(props) {
 
   props.cartContents.forEach((item, i) => {
     cartHTML.push(
-      <Grid key={i} container direction="row" justify="space-evenly" alignItems="center">
-        <Grid item xs={9}>
-          <Typography>
-            {item.name}
-          </Typography>
-        </Grid>
-        <Grid item xs={3}>
-          <Button 
-            onClick={(e) => {
-              props.dispatch({
-                type: 'REMOVE_FROM_CART',
-                payload: item
-              })
-            }}
-          >
-            <Delete />
-          </Button>
-        </Grid>
+      <Grid>
+        <Typography>
+          {item.name}
+        </Typography>
+
+        <Button
+          onClick={(e) => {
+            props.dispatch({
+              type: 'REMOVE_FROM_CART',
+              payload: item
+            })
+          }}
+        >
+          <Delete />
+        </Button>
       </Grid>
     )
   })
@@ -35,15 +32,14 @@ function SimpleCart(props) {
   return (
     <Accordion>
       <AccordionSummary
-          expandIcon={<ExpandMoreIcon />}
-          aria-controls="panel1a-content"
-        >
-          <Typography>Items</Typography>
+        expandIcon={<ExpandMoreIcon />}
+        aria-controls="additional-actions1-content"
+        id="additional-actions1-header"
+      >
+        <Typography>Items</Typography>
       </AccordionSummary>
       <AccordionDetails>
-        <Grid container>
-          { cartHTML }
-        </Grid>
+        {cartHTML}
       </AccordionDetails>
     </Accordion>
   )
